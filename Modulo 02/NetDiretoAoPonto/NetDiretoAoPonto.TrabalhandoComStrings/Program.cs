7cb1a757dbbc007e6a48c0b674f988a986cfc35d
Bruno E. M . Ferreira <brunoemf@gmail.com>
2021-10-28 14:40:01 -0300

    Aula Trabalhando com Strings - Principais Métodos - Prática

---
+++ Modulo 02/NetDiretoAoPonto/NetDiretoAoPonto.TrabalhandoComStrings/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace NetDiretoAoPonto.TrabalhandoComStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var paragrafo = "   C# é uma linguagem moderna e versátil. " +
+                "Com C# consigo desenvolver para Web, Desktop, Jogos," +
+                "Mobile, entre outros. ";
+
+
+            // Length
+            var tamanho = paragrafo.Length;
+
+            // Empty
+            var vazio = string.Empty;
+
+            // ToLower, ToUpper
+            var paragrafoMinusculo = paragrafo.ToLower();
+            var paragrafoMaiusculo = paragrafo.ToUpper();
+
+            // Split 
+            var frases = paragrafo.Split('.');
+
+            // Trim, TrimEnd, TrimStart
+            var pargrafoTrim = paragrafo.Trim();
+            var paragrafoTrimEnd = paragrafo.TrimEnd();
+            var paragrafoTrimStart = paragrafo.TrimStart();
+
+            // IsNullOrWhitespace
+            var isNullOrWhitespace = string.IsNullOrWhiteSpace(paragrafo);
+
+            // Replace
+            var paragrafoReplace = paragrafo.Replace("C#", "C-Sharp"); 
+
+
+
+        }
+    }
+}
